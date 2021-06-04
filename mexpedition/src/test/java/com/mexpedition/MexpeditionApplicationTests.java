4399db017af0ac73b67dc157fb090aa162c39839
Sadnelak <clartefelicien@hotmail.com>
2021-06-04 15:17:51 +0200

    init m-expedition

---
+++ mexpedition/src/test/java/com/mexpedition/MexpeditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mexpedition;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MexpeditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
